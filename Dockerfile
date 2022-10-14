4bd9d3e83c2f5dcc0b05eaff89a7cba1d033a3ae
Hajar Rashidi <hajarrashidi@gmail.com>
2022-10-14 20:35:07 +0200

    added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM php:8.1
+FROM mysql:8
+# PHP with cmd wont work if i dont use cli
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$message = 'Hello World';
+echo $message;
+# Steg.1 Vad är Docker
+// Docker är en öppen plattform för att bygga, skicka och köra applikationer
+// I Docker paketerar du dina applikationer med alla dess beroenden i en "låda" som kallas en container
+
+# Steg.2 containerizing {Your favorit framework}
+// För att containerize en applikation behöver du en Dockerfile
+// Dockerfile är en textfil som innehåller instruktioner för hur en container ska byggas
+
+# Steg.2
+# Koppla dig till databasen
+
+
+
